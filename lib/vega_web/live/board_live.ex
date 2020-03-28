06b4bcad6bd4ce98639c2996d0827d29ff14c214
Michael Maier <michael.maier@vivai.de>
2020-03-28 09:23:34 +0100

    refactored broadcasting updates

--- lib/vega_web/live/board_live.ex
+++ lib/vega_web/live/board_live.ex
@@ -1,11 +1,11 @@
 defmodule VegaWeb.BoardLive do
   use Phoenix.LiveView
 
-  alias Vega.Board
-  alias Vega.User
-  alias Vega.Issue
-  alias Vega.BoardList
   alias Phoenix.LiveView.Socket
+  alias Vega.Board
+  alias Vega.BoardList
+  alias Vega.Issue
+  alias Vega.User
 
   def mount(_params, session, socket) do
 
@@ -27,10 +27,10 @@ defmodule VegaWeb.BoardLive do
   Handle different messages from live components and broadcast messages from other nodes
   """
   def handle_info({:updated_board, board}, socket) do
-    {:noreply, assign(socket, board: broadcast_update(board), history: Issue.fetch_all(board))}
+    {:noreply, broadcast_update(socket, board)}
   end
-  def handle_info(%{event: "update-board", payload: board}, socket) do
-    {:noreply, assign(socket, board: board, history: Issue.fetch_all(board))}
+  def handle_info(%{event: "update-board", payload: %{board: board, history: history}}, socket) do
+    {:noreply, assign(socket, board: board, history: history)}
   end
 
   @doc"""
@@ -51,7 +51,7 @@ defmodule VegaWeb.BoardLive do
     Mongo.delete_many(:mongo, "boards", %{})
 
     board = create_example_board(assigns.current_user)
-    {:noreply, assign(socket, board: broadcast_update(board), history: Issue.fetch_all(board))}
+    {:noreply, broadcast_update(socket, board)}
   end
 
   def handle_event("edit", _value, socket) do
@@ -60,21 +60,19 @@ defmodule VegaWeb.BoardLive do
 
   def handle_event("save", %{"board" => %{"title" => new_title}}, %Socket{assigns: %{current_user: user, board: board}} = socket) do
     board = save_title(new_title, board, user)
-    history = Issue.fetch_all(board)
-    {:noreply, assign(socket, board: broadcast_update(board), edit: false, history: history)}
+    {:noreply, broadcast_update(socket, board, edit: false)}
   end
   def handle_event("save", %{"type" => "blur", "value" => new_title}, %Socket{assigns: %{current_user: user, board: board}} = socket) do
     board = save_title(new_title, board, user)
-    {:noreply, assign(socket, board: broadcast_update(board), edit: false, history: Issue.fetch_all(board))}
+    {:noreply, broadcast_update(socket, board, edit: false)}
   end
 
   def handle_event("move-list", %{"id" => id, "before" => before_id}, %Socket{assigns: %{board: board, current_user: current_user}} = socket) do
     with list when list != nil               <- Board.find_list(board, id),
          before_list when before_list != nil <- Board.find_list(board, before_id) do
 
-      board = board |> Board.move_list_before(current_user, list, before_list) |> broadcast_update()
-      {:noreply, assign(socket, board: board, history: Issue.fetch_all(board))}
-
+      board = Board.move_list_before(board, current_user, list, before_list)
+      {:noreply, broadcast_update(socket, board)}
     else
       _error -> {:noreply, socket}
     end
@@ -83,15 +81,15 @@ defmodule VegaWeb.BoardLive do
   def handle_event("move-list-to-end", id, %Socket{assigns: %{board: board, current_user: current_user}} = socket) do
     with list when list != nil <- Board.find_list(board, id) do
 
-      board = board |> Board.move_list_to_end(current_user, list) |> broadcast_update()
-      {:noreply, assign(socket, board: board, history: Issue.fetch_all(board))}
+      board = Board.move_list_to_end(board, current_user, list)
+      {:noreply, broadcast_update(socket, board)}
 
     else
       _error -> {:noreply, socket}
     end
   end
 
-  def handle_event("move-card", %{"id" => id, "to" => to_id, "from" => from_id, "before" => before_id} = params,
+  def handle_event("move-card", %{"id" => id, "to" => to_id, "from" => from_id, "before" => before_id},
                    %Socket{assigns: %{board: board, current_user: current_user}} = socket) do
 
     with to_list when to_list != nil         <- Board.find_list(board, to_id),
@@ -99,9 +97,8 @@ defmodule VegaWeb.BoardLive do
          card when card != nil               <- BoardList.find_card(from_list, id),
          before_card when before_card != nil <- BoardList.find_card(to_list, before_id) do
 
-      board = board |> Board.move_card_before(current_user, card, from_list, to_list, before_card) |> broadcast_update()
-      {:noreply, assign(socket, board: board, history: Issue.fetch_all(board))}
-
+      board = Board.move_card_before(board, current_user, card, from_list, to_list, before_card)
+      {:noreply, broadcast_update(socket, board)}
     else
       _error -> {:noreply, socket}
     end
@@ -115,8 +112,8 @@ defmodule VegaWeb.BoardLive do
          from_list when from_list != nil <- Board.find_list(board, from_id),
          card when card != nil           <- BoardList.find_card(from_list, id) do
 
-      board = board |> Board.move_card_to_end(current_user, card, from_list, to_list) |> broadcast_update()
-      {:noreply, assign(socket, board: board, history: Issue.fetch_all(board))}
+      board = Board.move_card_to_end(board, current_user, card, from_list, to_list)
+      {:noreply, broadcast_update(socket, board)}
 
     else
       _error -> {:noreply, socket}
@@ -176,11 +173,20 @@ defmodule VegaWeb.BoardLive do
   end
 
   ##
-  # Broadcast an update of the board
+  # Broadcast an update of the board and returns the updated assigns for the socket
   #
-  defp broadcast_update(board) do
-    VegaWeb.Endpoint.broadcast_from(self(), topic(board), "update-board", board)
-    board
+  defp broadcast_update(socket, board, assigns \\ [])
+  defp broadcast_update(socket, board, []) do
+    history = Issue.fetch_all(board)
+    VegaWeb.Endpoint.broadcast_from(self(), topic(board), "update-board", %{board: board, history: history})
+    assign(socket, board: board, history: history)
+  end
+  defp broadcast_update(socket, board, assigns) do
+    history = Issue.fetch_all(board)
+    VegaWeb.Endpoint.broadcast_from(self(), topic(board), "update-board", %{board: board, history: history})
+    socket
+    |> assign(board: board, history: history)
+    |> assign(assigns)
   end
 
   ##
